e75d637d9a51f902b1bfbf1683527b2bc660a965
gta191977649 <episodes27@gmail.com>
2018-11-06 00:14:10 +1100

    完成了nav导航

--- src/components/widgets/NavMenu.vue
+++ src/components/widgets/NavMenu.vue
@@ -4,7 +4,17 @@
         <div class="nav-menu-left">
             <div class="title">導航</div>
             <ul>
-                <li v-for="item in navmenu" :key="item.id"><a :href="handleLinkUri(item.object,item.url)">{{item.title}}</a></li>
+                <li v-for="item in getNavMenu(0)" :key="item.ID">
+                    <a href="#" v-on:click="handleLinkUri(item.object,item.url)">{{item.title}}</a>
+                    <!-- 便利继续查找属于该菜单的二级菜单 -->
+    
+                    <ul v-if="getNavMenu(item.ID)">
+                        <li v-for="secondItem in getNavMenu(item.ID)" :key="secondItem.ID">
+                            <a href="#" v-on:click="handleLinkUri(secondItem.object,secondItem.url)">{{secondItem.title}}</a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- HTML样式模板 
                 <li>
                     <a href="#">清单</a>
                     <ul>
@@ -12,7 +22,8 @@
                         <li><a href="#">课程记录</a></li>
                         <li><a href="#">其他</a></li>
                     </ul>
-                </li>         
+                </li>        
+                --> 
             </ul>
             
         </div>
@@ -29,12 +40,32 @@ export default {
             loaded: 'loaded'
         })
     },
+    mounted() {
+        this.getNavMenu(0);
+    },
     methods: {
         handleLinkUri(objecType,url) { //处理页面URI
-            if(objecType === 'page') { //当是个页面
-                alert(url.match('[^/]+(?=\/$|$)'));
-                return "#"
+            if(url) {
+                switch(objecType) {
+                    case 'custom': //外部链接
+                        return window.location.href = url
+                    case 'page': //页面指向
+                        let urlPostfix = url.match('[^/]+(?=\/$|$)')
+                        //console.log({name:'Page',params:{pageSlug:urlPostfix[0]}})
+                        return this.$router.push({name:'Page',params:{pageSlug:urlPostfix[0]}})
+                        
+                }
             }
+        },
+        getNavMenu(parent) { //根据Parent查询菜单
+            /*console.log("find ",parent)*/
+            let menu = []
+            this.navmenu.forEach(item => {
+                if(item.menu_item_parent == parent) {
+                    menu.push(item)
+                }
+            });
+            return menu
         }
     }
 }
