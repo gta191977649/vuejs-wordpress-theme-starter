94c7c409ce4c6ce7927d2b12be8cd0e3850d0b35
gta191977649 <episodes27@gmail.com>
2018-11-07 22:47:27 +1100

    添加了部分样式

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -2,8 +2,8 @@
     <div class="footer-area">
         <div class="props"></div>
         <div class="grassy"></div> 
-        <p>{{info.name}} {{ new Date().getFullYear() }} - Theme by Episodes <br/>Powered by Vue.js</p>
-        <p>当サイト内にある『MapleStory』に関連するコンテンツのデータ・画像等の著作権は、全て株式会社ネクソンおよび韓国NEXON社に帰属します</p>
+        <p><router-link to="/">{{info.name}} {{ new Date().getFullYear() }}</router-link> - Theme by Episodes <br/>Powered by <a href="https://cn.vuejs.org/">Vue.js</a> and <a href="https://wordpress.org/">Wordpress</a></p>
+        <p><small>(当サイト内にある『MapleStory』に関連するコンテンツのデータ・画像等の著作権は、全て株式会社ネクソンおよび韓国NEXON社に帰属します)</small></p>
     </div>
 </template>
 <script>
