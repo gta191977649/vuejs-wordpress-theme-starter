3f5158ff766f8b2bf6f29348c99c89c1bc8cd800
gta191977649 <episodes27@gmail.com>
2018-12-14 15:46:04 +1100

    添加了评论回复模板

--- functions.php
+++ functions.php
@@ -74,3 +74,32 @@ function ml_fix_search_url() {
     }   
 }
 add_action( 'template_redirect', 'ml_fix_search_url' );
+
+// 评论回应邮件通知
+// 评论回应邮件通知
+function comment_mail_notify($comment_id) {
+    $admin_email = get_bloginfo ('admin_email'); 
+    $comment = get_comment($comment_id);
+    $comment_author_email = trim($comment->comment_author_email);
+    $parent_id = $comment->comment_parent ? $comment->comment_parent : '';
+    $to = $parent_id ? trim(get_comment($parent_id)->comment_author_email) : '';
+    $spam_confirmed = $comment->comment_approved;
+    if (($parent_id != '') && ($spam_confirmed != 'spam') && ($to != $admin_email)) {
+      $wp_email = 'no-reply@' . preg_replace('#^www\.#', '', strtolower($_SERVER['SERVER_NAME']));
+      $subject = '您在 [' . get_option("blogname") . '] 的留言有了新回复';
+      $message = '<h1>您在 ' . get_option('blogname') . ' 博客上的留言有回复啦！</h1><hr/><div style="background-color:#FFF; border:1px solid #666666; color:#111; -moz-border-radius:8px; -webkit-border-radius:8px; -khtml-border-radius:8px; border-radius:8px; font-size:12px; width:702px; margin:0 auto; margin-top:10px; font-family:微软雅黑, Arial;">   <div style="background:#2c9b0a; width:100%; height:60px; color:white; -moz-border-radius:6px 6px 0 0; -webkit-border-radius:6px 6px 0 0; -khtml-border-radius:6px 6px 0 0; border-radius:6px 6px 0 0; "><span style="height:60px; line-height:60px; margin-left:30px; font-size:12px;">您在 <a style="text-decoration:none; color:#000;font-weight:600;" href="' . get_option('home') . '"><strong>' . get_option("blogname") . '</strong></a> 上<strong>《' . get_the_title($comment->comment_post_ID) . '》</strong>有一个评论回复</span></div>   
+      <div style="width:90%; margin:0 auto"><br/><p>您在<strong>《' . get_the_title($comment->comment_post_ID) . '》</strong>留下的评论是：</p>   
+      <p style="background-color: #EEE;border: 1px solid #DDD;padding: 20px;margin: 15px 0;">' . nl2br(get_comment($parent_id)->comment_content) . '</p>   
+      <p>' . trim($comment->comment_author) . ' 给您的新回复如下：<p style="background-color: #EEE;border: 1px solid #DDD;padding: 20px;margin: 15px 0;">' . nl2br($comment->comment_content) . '</p>   
+      <p>您可以点击“<a href="' . htmlspecialchars(get_comment_link($parent_id)) . '">这里</a>”查看评论   
+      </p>如果你不能点击上面的链接可以吧下面的链接复制到地址栏中访问   
+      <p style="background-color: #EEE;border: 1px solid #DDD;padding: 20px;margin: 15px 0;"><a href="' . htmlspecialchars(get_comment_link($parent_id)) . '">' . htmlspecialchars(get_comment_link($parent_id)) . '</a></p>   
+      <p>欢迎再次光临 <a style="text-decoration:none; color:#2c9b0a" href="' . get_option('home') . '">' . get_option('blogname') . '</a></p>   
+      <p>注意：此邮件由系统发出，请勿回复！</p>   
+      </div></div>';
+      $from = "From: \"" . get_option('blogname') . "\" <$wp_email>";
+      $headers = "$from\nContent-Type: text/html; charset=" . get_option('blog_charset') . "\n";
+      wp_mail( $to, $subject, $message, $headers );
+    }
+  }
+  add_action('comment_post', 'comment_mail_notify');
